0c0c8c4a63e66dc894c7b02de3dc0d8ab713787e
ShuheiInoguchi <32259@ojs.ac.jp>
2021-05-21 15:53:20 +0900

    P49 リスト2.4

--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -8,19 +8,26 @@ namespace DistanceConverter {
     class Program {
         static void Main(string[] args) {
             if (args.Length >= 1 && args[0] == "-tom") {
-                //フィートからメートルへの対応表を出力
-                for (int feet = 1; feet <= 10; feet++) {
-                    double meter = FeetToMeter(feet);
-                    Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
-                }
+                PrintFeetToMeterList(1, 30);
             } else {
-                //メートルからフィートへの対応表を出力
-                for (int meter = 1; meter <= 10; meter++) {
-                    double feet = MeterToFeet(meter);
-                    Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
-                }
+                PrintMeterToFeetList(1, 30);
             }
         }
+        private static void PrintFeetToMeterList(int start, int stop) {
+            //フィートからメートルへの対応表を出力
+            for (int feet = start; feet <= stop; feet++) {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+            }
+        }       
+        private static void PrintMeterToFeetList(int start, int stop) {
+            //メートルからフィートへの対応表を出力
+            for (int meter = start; meter <= stop; meter++) {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
+            }
+        }
+
         //フィートからメートルを求める 
         private static double FeetToMeter(int feet) {
             return feet * 0.3048;
