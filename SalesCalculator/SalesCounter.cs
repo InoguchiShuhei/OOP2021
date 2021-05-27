595f1b9c78fdc04bfdbbf762fc3df65e36cb3ca6
ShuheiInoguchi <32259@ojs.ac.jp>
2021-05-27 15:56:02 +0900

    P70リスト2.22

--- SalesCalculator/SalesCounter.cs
+++ SalesCalculator/SalesCounter.cs
@@ -13,5 +13,19 @@ namespace SalesCalculator {
         public SalesCounter(List<Sale> sales) {
             _sales = sales;
         }
+
+        //List2.17
+        //店舗別売り上げを求める
+        public Dictionary<string, int> GetPerStoreSales() {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (Sale sale in _sales) {
+                if (dict.ContainsKey(sale.ShopName))
+                    //既にコレクションに(dictionary)店舗が設定されている
+                    dict[sale.ShopName] += sale.amount;
+                else
+                    dict[sale.ShopName] = sale.amount;
+            }
+            return dict;
+        }
     }
 }
