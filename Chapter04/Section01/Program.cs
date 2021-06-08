adbaa890e8cb8434e84856eaafd72165dbfd126a
ShuheiInoguchi <32259@ojs.ac.jp>
2021-06-08 11:52:45 +0900

    P125 その他のイディオム

--- Chapter04/Section01/Program.cs
+++ Chapter04/Section01/Program.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
+            int count = 0;
+
+            Console.WriteLine($"後置 : {count++}");
+
+            Console.WriteLine($"前置 : {++count}");
+
         }
     }
 }
+
