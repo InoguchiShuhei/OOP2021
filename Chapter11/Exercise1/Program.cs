e78ff5ff4f6f1887870b2bc3aec4cfb32a4fd055
修平 井野口 <32259@ojs.ac.jp>
2021-08-30 15:34:06 +0900

    11.1.4

--- Chapter11/Exercise1/Program.cs
+++ Chapter11/Exercise1/Program.cs
@@ -20,6 +20,9 @@ namespace Exercise1
 
             Exercise1_3(file);
             Console.WriteLine("-------");
+
+            Exercise1_4(file);
+            Console.WriteLine("-------");
         }
 
         private static void Exercise1_1(string file)
@@ -60,5 +63,18 @@ namespace Exercise1
 
             Console.WriteLine("{0}", ballsports.Name);
         }
+
+            private static void Exercise1_4(string file)
+            {
+                var newfile = "sports.xml";
+                var xdoc = XDocument.Load(file);
+                var element = new XElement("ballsport",
+                     new XElement("name", "サッカー", new XAttribute("kanji", "蹴球")),
+                     new XElement("teammembers", "11"),
+                     new XElement("firstplayed", "1863")
+                  );
+                xdoc.Root.Add(element);
+                xdoc.Save(newfile);
+            }
     }
 }
