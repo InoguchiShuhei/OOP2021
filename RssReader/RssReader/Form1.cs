6e5df09393cd467b03f7ad8c9aea8d6fffc8adf7
修平 井野口 <32259@ojs.ac.jp>
2021-09-06 16:00:48 +0900

    タイトル途中

---
+++ RssReader/RssReader/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader
+{
+    public partial class Form1 : Form
+    {
+        private object cityCode;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btRead_Click(object sender, EventArgs e)
+        {
+            setRssTitle(tbUrl.Text);
+        }
+
+        //指定したURL先に
+        private void setRssTitle(string uri)
+        {
+            using (var wc = new WebClient())
+            {
+                wc.Headers.Add("Content-type", "charset=UTF-8");
+                //var uriString = string.Format(
+                    //@"http://rss.weather.yahoo.co.jp/rss/days/{0}.xml");
+                var url = new Uri(uri);
+                var stream = wc.OpenRead(url);
+
+                XDocument xdoc = XDocument.Load(stream);
+                var nodes = xdoc.Root.Descendants("title");
+                foreach (var node in nodes)
+                {
+                    string s = Regex.Replace(node.Value, "【|】", "");
+                }
+            }
+        }
+    }
+}
